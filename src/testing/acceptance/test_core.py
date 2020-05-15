8686b5cb511b5b24ab3bfcaaac1cde8881e393c5
Miguel Trujillo <miguel@cloudsigma.com>
2020-05-14 23:28:23 -0500

    Refactor test_core utils to be compatible with Fabric 2

--- src/testing/acceptance/test_core.py
+++ src/testing/acceptance/test_core.py
@@ -130,44 +130,34 @@ class TestCoreFuncs(common.StatefulResourceTestBase):
         self._wait_deleted(d1['uuid'], client=dc)
         self._wait_deleted(d2_uuid, client=dc)
 
-    def get_single_ctx_val(self, command, expected_val, fab_kwargs, ip1, fabric_settings, tasks, api):
-        with fabric_settings(**fab_kwargs):
-            # TODO: Remove this retry when proper guest context client is implemented
-            res_string = None
-            for retry in xrange(5):
-                if retry > 0:
-                    LOG.warning('Retrying guest context single value execution {}'.format(retry))
-                ctx_val_res = tasks.execute(
-                    api.run,
-                    command,
-                    hosts=["root@%s" % ip1]
-                )
+    def get_single_ctx_val(self, command, expected_val, conn):
+        # TODO: Remove this retry when proper guest context client is implemented
+        res_string = None
+        for retry in range(5):
+            if retry > 0:
+                LOG.warning('Retrying guest context single value execution {}'.format(retry))
+            ctx_val_res = conn.run(command)
 
-                res_string = ctx_val_res.values()[0]
-                if res_string == expected_val:
-                    break
+            res_string = ctx_val_res.stdout.rstrip()
+            if res_string == expected_val:
+                break
         return res_string
 
-    def get_full_ctx(self, command, fab_kwargs, ip1, fabric_settings, tasks, api):
-        with fabric_settings(**fab_kwargs):
-            res_string = ''
-            # TODO: Remove this retry when proper guest context client is implemented
-            ctx_res_json = {}
-            for retry in xrange(5):
-                if retry > 0:
-                    LOG.warning('Retrying guest context whole definition execution {}'.format(retry))
-                try:
-                    ctx_res = tasks.execute(
-                        api.run,
-                        command,
-                        hosts=["root@%s" % ip1]
-                    )
-                    res_string = ctx_res.values()[0]
-                    ctx_res_json = json.loads(res_string)
-                except:
-                    continue
-                else:
-                    break
+    def get_full_ctx(self, command, conn):
+        res_string = ''
+        # TODO: Remove this retry when proper guest context client is implemented
+        ctx_res_json = {}
+        for retry in range(5):
+            if retry > 0:
+                LOG.warning('Retrying guest context whole definition execution {}'.format(retry))
+            try:
+                ctx_res = conn.run(command)
+                res_string = ctx_res.stdout.rstrip()
+                ctx_res_json = json.loads(res_string)
+            except:
+                continue
+            else:
+                break
 
         return ctx_res_json, res_string
 
@@ -177,19 +167,19 @@ class TestCoreFuncs(common.StatefulResourceTestBase):
         with open(os.path.join(dump_path, 'response_' + op_name), 'w') as dump_file:
             dump_file.write(res_string)
 
-    def check_key_retrieval(self, g_def, op_name, ctx_path, dump_path, fab_kwargs, ip1, fabric_settings, tasks, api):
+    def check_key_retrieval(self, g_def, op_name, ctx_path, dump_path, conn):
         command = self.command_template.format(ctx_path)
         expected_val = g_def
         for path_el in ctx_path.split('/'):
             if path_el:  # non-empty string
                 expected_val = expected_val.get(path_el)
-        res_string = self.get_single_ctx_val(command, expected_val, fab_kwargs, ip1, fabric_settings, tasks, api)
+        res_string = self.get_single_ctx_val(command, expected_val, conn)
         self.assertEqual(res_string, expected_val)
         self.dump_ctx_command(command, res_string, op_name, dump_path)
 
-    def check_all_retrieval(self, g_def, op_name, dump_path, fab_kwargs, ip1, fabric_settings, tasks, api):
+    def check_all_retrieval(self, g_def, op_name, dump_path, conn):
         command = self.command_template.format('')
-        ctx_res_json, res_string = self.get_full_ctx(command, fab_kwargs, ip1, fabric_settings, tasks, api)
+        ctx_res_json, res_string = self.get_full_ctx(command, conn)
         for k, v in g_def.items():
             if not isinstance(v, (list, dict)):
                 self.assertEqual(v, ctx_res_json[k])
@@ -255,85 +245,38 @@ class TestCoreFuncs(common.StatefulResourceTestBase):
         LOG.debug('Get the assigned ips')
         ip1 = g1['nics'][0]['runtime']['ip_v4']["uuid"]
 
-        self._wait_for_open_socket(ip1, 22, timeout=60, close_on_success=True)
+        self._wait_for_open_socket(ip1, 22, timeout=90, close_on_success=True)
 
-        from fabric.api import settings as fabric_settings
-        from fabric import tasks, api
+        from fabric import Connection
 
-        fab_kwargs = {
-            "warn_only": True,
-            "abort_on_prompts": True,
-            "use_ssh_config": p_pass is None
-        }
-        LOG.debug('Using fabric config {}'.format(fab_kwargs))
-        if p_pass is not None:
-            fab_kwargs['password'] = p_pass
-            LOG.debug('Using a password to SSH to the servers ( not using ssh config )')
-
+        fkwargs = {'password': p_pass}
+        conn = Connection(ip1, user='root', connect_kwargs=fkwargs)
         dump_path = dump_response.response_dump.dump_path
-
-        #command_template = r"read -t 1 -d $'\004' DISCARD < /dev/ttyS1; " \
-        #                   r'echo -en "<\n{}\n>" > /dev/ttyS1 && read -t 3 READVALUE < /dev/ttyS1 && echo $READVALUE'
         self.command_template = r'v=$(read -t 13 READVALUE < /dev/ttyS1 && echo $READVALUE & sleep 1; echo -en "<\n{}\n>" > /dev/ttyS1; wait %1); echo $v'
 
         LOG.debug('Test the guest context')
+        self.check_key_retrieval(g_def, 'context_single_value', 'name', dump_path, conn)
+        self.check_key_retrieval(g_def, 'context_single_value_ssh_key', '/meta/ssh_public_key', dump_path, conn)
+        self.check_all_retrieval(g_def, 'context_all', dump_path, conn)
 
-        LOG.debug('Check single value retrieval')
-        self.check_key_retrieval(g_def, 'context_single_value', 'name', dump_path, fab_kwargs, ip1, fabric_settings,
-                                 tasks, api)
-
-
-        ##########################################
-        LOG.debug('Check key retrieval')
-        self.check_key_retrieval(g_def, 'context_single_value_ssh_key', '/meta/ssh_public_key', dump_path, fab_kwargs, ip1,
-                         fabric_settings, tasks, api)
-
-        ##########################################
-        LOG.debug('Check complete context retrieval')
-        self.check_all_retrieval(g_def, 'context_all', dump_path, fab_kwargs, ip1, fabric_settings, tasks, api)
-
-        ##########################################
-        ##########################################
-        ##########################################
-        ##########################################
         LOG.debug('Check context dynamic update')
         g_def['name'] += '_renamed'
         g_def['meta']['another_key'] = 'a value or something'
-
         upd_res = sc.update(g1['uuid'], g_def)
         self.assertEqual(g_def['name'], upd_res['name'])
-
-        LOG.debug('Check single value retrieval')
-
-        self.check_key_retrieval(g_def, 'context_single_value_dynamic', 'name', dump_path, fab_kwargs, ip1,
-                                fabric_settings, tasks, api)
-
-        ##########################################
-        LOG.debug('Check key retrieval')
-        self.check_key_retrieval(g_def, 'context_single_value_another_key_dynamic', '/meta/another_key', dump_path,
-                                 fab_kwargs, ip1, fabric_settings, tasks, api)
-
-        ##########################################
-        LOG.debug('Check complete context retrieval')
-        self.check_all_retrieval(g_def, 'context_all_dynamic', dump_path, fab_kwargs, ip1, fabric_settings, tasks, api)
-
-        ###########################################
-        ###########################################
-        ###########################################
+        self.check_key_retrieval(g_def, 'context_single_value_dynamic', 'name', dump_path, conn)
+        self.check_key_retrieval(g_def, 'context_single_value_another_key_dynamic', '/meta/another_key', dump_path, conn)
+        self.check_all_retrieval(g_def, 'context_all_dynamic', dump_path, conn)
         with dump_response('update_global_context'):
             gcc.update({'new_global_key': 'new_global_val'})
 
         LOG.debug('Check global context retrieval')
         command = self.command_template.format('/global_context/new_global_key')
         expected_val = 'new_global_val'
-        res_string = self.get_single_ctx_val(command, expected_val, fab_kwargs, ip1, fabric_settings, tasks, api)
+        res_string = self.get_single_ctx_val(command, expected_val, conn)
         self.assertEqual(res_string, expected_val)
         self.dump_ctx_command(command, res_string, 'global_context_single_value', dump_path)
-
-        self.check_all_retrieval(g_def, 'global_context_all', dump_path, fab_kwargs, ip1, fabric_settings, tasks, api)
-
-
-
+        self.check_all_retrieval(g_def, 'global_context_all', dump_path, conn)
 
         LOG.debug('Stopping guest')
         sc.stop(g1['uuid'])
@@ -345,7 +288,6 @@ class TestCoreFuncs(common.StatefulResourceTestBase):
         LOG.debug('Delete drive')
         dc.delete(d1['uuid'])
         self._wait_deleted(d1['uuid'], client=dc)
-
 
 
     @skip("Temporary skipping inconsistent tests")
